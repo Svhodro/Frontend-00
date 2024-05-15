a450f24740528cd1a7b4cd93049fb07cb6989c39
Svhodro <rudrolipi@gmail.com>
2024-05-16 00:24:31 +0600

    docs- add navber

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom"
 import Footer from "../pages/Home/Footer/Footer"
+import Nav from "../pages/Home/Navber/Nav"
 
 
 function Main() {
   return (
     <div>
+        <Nav/>
         <Outlet/>
         <Footer/>
     </div>
