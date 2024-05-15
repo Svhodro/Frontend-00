e5a4d4f1fffb6b141dbb45dcab1be6c183671300
Svhodro <rudrolipi@gmail.com>
2024-05-16 00:52:12 +0600

    docs- add banner and chane nav code

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Hero from '../Banner/Hero'
 
 function Home() {
   return (
-    <div>Home page jai sri ram</div>
+    <div>
+        <Hero/>
+    </div>
   )
 }
 
--- src/pages/Home/Navber/Nav.jsx
+++ src/pages/Home/Navber/Nav.jsx
@@ -17,7 +17,7 @@ function Nav() {
                 </>
     return (
         <div>
-            <div className="navbar bg-base-100">
+            <div className="navbar bg-black fixed z-10 bg-opacity-65 max-w-7xl text-white ">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
